7e1600585ee931bc10157fa8ed832ff21d1e24e8
Naoki Yasuraoka <yasuraok@gmail.com>
2023-06-16 01:59:29 +0000

    Add hook from dotfiles that runs only once after devcontainer started

--- install.sh
+++ install.sh
@@ -9,10 +9,24 @@ if ! grep -q "^# auto ssh-add$" ~/.bashrc; then
   } >> ~/.bashrc
 fi
 
-# setup devcontainer
+# devcontainer固有の設定
 if [ -n "$REMOTE_CONTAINERS" ]; then
   sudo apt update
   sudo apt install -y vim git bash-completion
-  git config --global core.editor vim
-  code --install-extension eamodio.gitlens
+  
+  # dotfiles/install.shのタイミングでは早すぎて動かない処理をスクリプトに書き出す
+  {
+    echo "git config --global core.editor vim";
+    echo "code --install-extension eamodio.gitlens";
+  } >> ~/dotfiles/devcontainer_init.sh
+  
+  # devcontainer起動後に一回だけ上記を実行するように.bashrcに仕込む
+  {
+    echo "";
+    echo "# hook only once after devcontainer created";
+    echo 'if [ -f "${HOME}/dotfiles/devcontainer_init.sh" ]; then'
+    echo '  bash -xe ${HOME}/dotfiles/devcontainer_init.sh'
+    echo '  rm ${HOME}/dotfiles/devcontainer_init.sh'
+    echo 'fi'
+  } >> ~/.bashrc
 fi
