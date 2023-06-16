36f4b20533e2bfc43e91eb4507437e45f8a3fe98
Naoki Yasuraoka <yasuraok@gmail.com>
2023-06-16 10:10:26 +0900

    install gitlens extension

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xe
 
+# auto ssh-add
 if ! grep -q "^# auto ssh-add$" ~/.bashrc; then
   {
     echo "";
@@ -8,6 +9,10 @@ if ! grep -q "^# auto ssh-add$" ~/.bashrc; then
   } >> ~/.bashrc
 fi
 
-sudo apt update
-sudo apt install -y vim git bash-completion
-git config --global core.editor vim
+# setup devcontainer
+if [ -n "$REMOTE_CONTAINERS" ]; then
+  sudo apt update
+  sudo apt install -y vim git bash-completion
+  git config --global core.editor vim
+  code --install-extension eamodio.gitlens
+fi
