d4d46659dbaf22de39010c912f5b25b39a4c0e97
Naoki Yasuraoka <yasuraok@gmail.com>
2023-07-27 15:01:22 +0900

    Update extensions

--- install.sh
+++ install.sh
@@ -18,8 +18,9 @@ if [ -n "$REMOTE_CONTAINERS" ]; then
   {
     echo "git config --global core.editor vim";
     echo "code --install-extension mhutchie.git-graph";
-    echo "code --install-extension genieai.chatgpt-vscode";
+    echo "code --install-extension yzhang.markdown-all-in-one";
     echo "code --install-extension dzhavat.css-flexbox-cheatsheet";
+    echo "code --install-extension genieai.chatgpt-vscode";
     echo "code --install-extension gitHub.copilot";
     echo "code --install-extension github.copilot-chat";
   } >> ~/dotfiles/devcontainer_init.sh
