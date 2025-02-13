7ed3a1320ec1c7d316b2b6aa469608d801871ccc
Naoki Yasuraoka <yasuraok@gmail.com>
2025-02-13 12:36:00 +0900

    Add rclone serve script

---
+++ yash/serve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ue
+
+source "$(dirname $0)/.env"
+
+CMD1="rclone mount --vfs-cache-mode full combine: R:"
+CMD2="rclone serve sftp --addr :14240 --user raoka --pass ${RAOKA_PASS} --vfs-cache-mode off R: -v"
+CMD3="rclone serve sftp --addr :24240 --user a4yg  --pass ${A4YG_PASS}  --vfs-cache-mode off pcloud_enc_private:"
+
+wt new-tab ${CMD1} \; split-pane -H --size 0.66 ${CMD2} \; split-pane -H --size 0.50 ${CMD3}
