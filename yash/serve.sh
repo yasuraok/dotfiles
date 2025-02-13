65e50706910feb3904f2b99ac60c3f0c35a0b873
Naoki Yasuraoka <yasuraok@gmail.com>
2025-02-13 14:54:41 +0900

    Set max-age option long

--- yash/serve.sh
+++ yash/serve.sh
@@ -2,8 +2,9 @@
 
 source "$(dirname $0)/.env"
 
-CMD1="rclone mount --links --vfs-cache-mode full --vfs-cache-min-free-space 10G combine: R:"
-CMD2="rclone serve sftp --addr :14240 --user raoka --pass ${RAOKA_PASS} --vfs-cache-mode off R: -v"
-CMD3="rclone serve sftp --addr :24240 --user a4yg  --pass ${A4YG_PASS}  --vfs-cache-mode off pcloud_enc_private:"
+CACHE_OPT=(--vfs-cache-mode full --vfs-cache-min-free-space 10G --vfs-cache-max-age 180d)
+CMD1=(rclone mount --links "${CACHE_OPT[@]}" combine: R:)
+CMD2=(rclone serve sftp --addr :14240 --user raoka --pass ${RAOKA_PASS} "${CACHE_OPT[@]}"    combine: -v)
+CMD3=(rclone serve sftp --addr :24240 --user a4yg  --pass ${A4YG_PASS}  --vfs-cache-mode off pcloud_enc_private:)
 
-wt new-tab ${CMD1} \; split-pane -H --size 0.66 ${CMD2} \; split-pane -H --size 0.50 ${CMD3}
+wt new-tab "${CMD1[@]}" \; split-pane -H --size 0.66 "${CMD2[@]}" \; split-pane -H --size 0.50 "${CMD3[@]}"
