a169f9fd46f51244ce0b3c1ca27b32562254a692
Naoki Yasuraoka <yasuraok@gmail.com>
2025-02-13 12:51:38 +0900

    Add cache option

--- yash/serve.sh
+++ yash/serve.sh
@@ -2,7 +2,7 @@
 
 source "$(dirname $0)/.env"
 
-CMD1="rclone mount --vfs-cache-mode full combine: R:"
+CMD1="rclone mount --links --vfs-cache-mode full --vfs-cache-min-free-space 10G combine: R:"
 CMD2="rclone serve sftp --addr :14240 --user raoka --pass ${RAOKA_PASS} --vfs-cache-mode off R: -v"
 CMD3="rclone serve sftp --addr :24240 --user a4yg  --pass ${A4YG_PASS}  --vfs-cache-mode off pcloud_enc_private:"
 
