0e541ef7004b193880dad8273223b0a09e0548b3
Naoki Yasuraoka <yasuraok@gmail.com>
2025-03-30 10:07:38 +0900

    Split lines

--- yash/serve.sh
+++ yash/serve.sh
@@ -17,4 +17,7 @@ CMD2=(rclone serve sftp --addr :${SFTP1_PORT} --user ${SFTP1_USER} --pass ${SFTP
 CMD3=(rclone serve sftp --addr :${SFTP2_PORT} --user ${SFTP2_USER} --pass ${SFTP2_PASS} --vfs-cache-mode off ${SFTP2_SOURCE})
 CMD4=(powershell -ExecutionPolicy RemoteSigned -File "$(cygpath -w "$DIR" | sed 's/\\/\//g')/disable_restart.ps1")
 
-wt new-tab "${CMD1[@]}" \; split-pane -H --size 0.75 "${CMD2[@]}" \; split-pane -H --size 0.66 "${CMD3[@]}" \; split-pane -H --size 0.5 "${CMD4[@]}"
+wt new-tab "${CMD1[@]}" \; \
+  split-pane -H --size 0.75 "${CMD2[@]}" \; \
+  split-pane -H --size 0.66 "${CMD3[@]}" \; \
+  split-pane -H --size 0.50 "${CMD4[@]}"
