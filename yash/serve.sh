ea8b6ade0c0116be42e34019c69d56c734dcdfc3
Naoki Yasuraoka <yasuraok@gmail.com>
2025-03-31 09:33:28 +0900

    Change to webdav for macOS

--- yash/serve.sh
+++ yash/serve.sh
@@ -13,8 +13,8 @@ else
 fi
 
 CMD1=(rclone mount --links "${CACHE_OPT[@]}" combine: R: --network-mode ${V_OPT})
-CMD2=(rclone serve sftp --addr :${SFTP1_PORT} --user ${SFTP1_USER} --pass ${SFTP1_PASS} --vfs-cache-mode off ${SFTP1_SOURCE} ${V_OPT})
-CMD3=(rclone serve sftp --addr :${SFTP2_PORT} --user ${SFTP2_USER} --pass ${SFTP2_PASS} --vfs-cache-mode off ${SFTP2_SOURCE})
+CMD2=(rclone serve webdav --addr :${SFTP1_PORT} --user ${SFTP1_USER} --pass ${SFTP1_PASS} --vfs-cache-mode off ${SFTP1_SOURCE} ${V_OPT})
+CMD3=(rclone serve sftp   --addr :${SFTP2_PORT} --user ${SFTP2_USER} --pass ${SFTP2_PASS} --vfs-cache-mode off ${SFTP2_SOURCE})
 CMD4=(powershell -ExecutionPolicy RemoteSigned -File "$(cygpath -w "$DIR" | sed 's/\\/\//g')/disable_restart.ps1")
 
 wt new-tab "${CMD1[@]}" \; \
